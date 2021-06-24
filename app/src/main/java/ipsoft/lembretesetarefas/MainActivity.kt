f23fa41f3a7f2af1479de015bbfaa15cbc06d458
Anthoni Ipiranga <anthoni.ipiranga@gmail.com>
2021-06-24 00:17:37 -0300

    First Commit

---
+++ app/src/main/java/ipsoft/lembretesetarefas/MainActivity.kt
@@ -0,0 +1,18 @@
+package ipsoft.lembretesetarefas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ipsoft.lembretesetarefas.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
